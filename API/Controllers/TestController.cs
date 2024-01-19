85b19662b5b904dce28feef15058eb1e2eee88ac
piotrbar250 <piotr.baranowski250@gmail.com>
2024-01-19 03:46:30 +0100

    test change

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Orders;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public string test(int id)
+        {
+            return "Works fine";
+        }
+    }
+}
